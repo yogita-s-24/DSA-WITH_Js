95bdaec4843ac8b9f52d6e7c995c4a0a80a23356
Yogita Shete <yogitashete24@gmail.com>
2025-02-24 22:55:11 +0530

    fix: correct function call for finding and logging girl's name

--- Array/index.js
+++ Array/index.js
@@ -1,13 +1,23 @@
 const studentData = ["Yogita", "Rahul", "Yuvraj", "Sayali"];
 
 const findStudent = (allStudents, studentName) =>{
-
     for(let i=0; i<allStudents.length; i++){
         if(allStudents[i] === studentName){
             console.log(studentName)
         }
     }
-
 }
 
-findStudent(studentData, "Yogita")
+findStudent(studentData, "Yogita");
+
+
+const girlData = ["Ira", "Riya", "Kiya", "Ria"];
+
+const findGirls = (allGirls, girlsName) => {
+    for(let i=0; i<allGirls.length; i++){
+        if(allGirls[i] === girlsName){
+            console.log(girlsName)
+        }
+    }
+}
+findGirls(girlData, "Ira");
