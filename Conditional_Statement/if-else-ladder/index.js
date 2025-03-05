88b215a8f5d74063933f66aed76c8965ad30c808
Yogita Shete <yogitashete24@gmail.com>
2025-03-05 20:47:30 +0530

    Refactor discount calculation by reducing redundancy

--- Conditional_Statement/if-else-ladder/index.js
+++ Conditional_Statement/if-else-ladder/index.js
@@ -1,17 +1,39 @@
+// const amount = Number(prompt("What is the final amount??"));
+
+// if(amount > 0 && amount <= 5000){
+//     console.log(amount)
+// }
+
+// else if(amount > 5000 && amount <= 7000){
+//     console.log(amount - Math.floor((5 * amount) / 100));
+// }
+
+// else if(amount > 7000 && amount <= 9000){
+//     console.log(amount - Math.floor((10 * amount) / 100));
+// }
+
+// else if(amount > 9000){
+//     console.log(amount - Math.floor((20 * amount) / 100))
+// }
+
 const amount = Number(prompt("What is the final amount??"));
 
+let dis = 0;
+
 if(amount > 0 && amount <= 5000){
-    console.log(amount)
+    dis = 0
 }
 
 else if(amount > 5000 && amount <= 7000){
-    console.log(amount - Math.floor((5 * amount) / 100));
+    dis = 5
 }
 
 else if(amount > 7000 && amount <= 9000){
-    console.log(amount - Math.floor((10 * amount) / 100));
+    dis = 10;
 }
 
 else if(amount > 9000){
-    console.log(amount - Math.floor((20 * amount) / 100))
+    dis = 20;
 }
+
+console.log(amount - Math.floor((dis * amount)/100));
