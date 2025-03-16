cde30888eb61cc729188338fbbea97a8a5955b2f
Yogita Shete <yogitashete24@gmail.com>
2025-03-16 14:18:17 +0530

    Feat: Implement function to find and log all unique pairs in an array

---
+++ src/For_Loop/pair.js
@@ -0,0 +1,10 @@
+ function findPairs (arr){
+    for(let i=0; i<arr.length; i++){
+        for(let j=i+1; j<arr.length; j++){
+            console.log(`Pair : ${i}, ${j}`)
+        }
+    }
+ }
+
+ const number = [1,2,3,4,5,6,7,8,9]
+ findPairs(number)
