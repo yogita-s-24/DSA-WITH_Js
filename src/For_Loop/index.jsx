5c6181574e5782b99dc96084a4db29e1a57ad5d0
Yogita Shete <yogitashete24@gmail.com>
2025-03-19 22:32:03 +0530

    Add sum calculation with user input via prompt in React component

--- src/For_Loop/index.jsx
+++ src/For_Loop/index.jsx
@@ -1,25 +1,35 @@
 import React from 'react'
 
 const index = () => {
-    var n = Number(prompt("Kaha tak add karenge?"));
+  var pr = prompt("Kaha tk add karwaoge??");
 
-if (isNaN(n)) {
-  console.log("Invalid input");
-} else {
-  if (n > 0) {
-    var sum = 0;
-    for (var i = 0; i <= n; i++) {
-      sum = sum + i;
-    }
-    console.log(sum);
-  } else {
-    console.log("should be +ve and more than 0");
-  }
+if(pr === null){
+  console.log("Cancelled");
 }
 
+else{
+  var n = Number(pr);
+
+  if(isNaN(n)){
+    console.log("Invalid Input");
+  }
+
+  else{
+    if(n>0){
+      var sum = 0;
+      for(var i=0; i<=n; i++){
+        sum = sum + i;
+      }
+      console.log(sum);
+    }
+    else{
+      console.log(`Should be +ve and more than 0`);
+    }
+  }
+}
   return (
     <div>
-      for
+      For
     </div>
   )
 }
