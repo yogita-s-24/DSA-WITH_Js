352db6ac3991ef8e7661ce4a7e207bebfb7487e5
Yogita Shete <yogitashete24@gmail.com>
2025-03-21 16:10:53 +0530

    create program for find factorial number

---
+++ src/For_Loop/factorial.js
@@ -0,0 +1,23 @@
+var pr = 5;
+
+if (pr === null) {
+  console.log("Cancelled");
+} 
+else {
+  let n = Number(pr);
+
+    if (isNaN(n)) {
+    console.log("Invalid Input");
+  } 
+    else {
+
+    if (n > 0) {
+      let fact = 1;
+
+      for (let i = 1; i <= n; i++) {
+        fact = fact * i;
+      }
+      console.log(`Factorial of ${pr} is ${fact}`);
+    }
+  }
+}
