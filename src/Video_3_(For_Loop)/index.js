7375511a429d1abe7aa28ea9bcd6700a144a63cb
Yogita Shete <yogitashete24@gmail.com>
2025-04-05 18:42:03 +0530

    Added prime number check with optimized isPrimeNumber function using square root logic

--- src/Video_3_(For_Loop)/index.js
+++ src/Video_3_(For_Loop)/index.js
@@ -1,22 +1,42 @@
-let pr = 100;
+let pr = 977;
 
-if (pr === null) {
+if(pr===null){
   console.log("Cancelled");
-} else {
-  var n = Number(pr);
+}
 
-  if (isNaN(n)) {
-    console.log("Invalid Input");
-  } else {
-    if (n > 0) {
-      for (let i = 1; i <= Math.floor(n / 2); i++) {
-        if (n % i === 0) {
-          console.log(i);
-        }
-      }
-      console.log(n);
-    } else {
-      console.log("Should be positive and more than 0");
+else{
+  let n = Number(pr);
+
+  if(isNaN(n)){
+    console.log("Invalid Input")
+  }
+  else{
+    if(n>0){
+      // let isPrime = true;
+      // for(let i=2; i<=Math.floor(n/2); i++){
+      //   if(n % i === 0){
+      //     isPrime = false;
+      //     break;
+      //   }
+      // }
+      // console.log(isPrime);
+
+      console.log(isPrimeNumber(n));
+    }
+
+    else{
+      console.log("Should ne positive and more than 0");
     }
   }
+}
+
+function isPrimeNumber(n){
+  if (n<=1) return false;
+  if(n==2) return true;
+  if(n%2==0) return false;
+
+  for(let i=3; i<=Math.floor(Math.sqrt(n)); i+=2){
+    if(n%1==0) return false
+  }
+  return true
 }
