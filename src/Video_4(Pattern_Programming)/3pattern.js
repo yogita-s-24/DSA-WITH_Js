9f89a8cb3a6bba26ae44f4043a2b3d0960281eea
Yogita Shete <yogitashete24@gmail.com>
2025-05-10 08:29:53 +0530

    feat: print right-angled triangle pattern with increasing numbers

---
+++ src/Video_4(Pattern_Programming)/3pattern.js
@@ -0,0 +1,14 @@
+import promptSync from "prompt-sync";
+
+const prompt = promptSync();
+
+let n = Number(prompt("Enter the number"));
+
+for(let i=1; i<=n; i++){
+    for(let j=1; j<=i; j++){
+        // eslint-disable-next-line no-undef
+        process.stdout.write(j + " ");
+    }
+    console.log(" ");
+    
+}
