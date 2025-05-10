d386097b8d7a6fa6a9d30af1abe76b2a65b271cc
Yogita Shete <yogitashete24@gmail.com>
2025-05-10 08:14:43 +0530

    feat: generate square pattern of asterisks based on user input

---
+++ src/Video_4(Pattern_Programming)/1pattern.js
@@ -0,0 +1,17 @@
+//square patter
+
+import promptSync from 'prompt-sync';
+
+const prompt = promptSync();
+
+let n = Number(prompt("Enter the number"));
+
+for(let i=1; i<=n; i++){
+    for(let j=1; j<=n; j++){
+        // eslint-disable-next-line no-undef
+        process.stdout.write("* ")
+    }
+    console.log(" ");
+    
+}
+
