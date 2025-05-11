955c4ab0290a32956fc62754109a92411e640ca8
Yogita Shete <yogitashete24@gmail.com>
2025-05-11 16:37:49 +0530

    Refactor star pattern script using reverse loop for improved readability

--- src/Video_4(Pattern_Programming)/5pattern.js
+++ src/Video_4(Pattern_Programming)/5pattern.js
@@ -1,14 +1,26 @@
   import promptSync from "prompt-sync";
-
   const prompt = promptSync();
 
-  let n = Number(prompt("Enter the number :"));
+//   let n = Number(prompt("Enter the number :"));
+//   for(let i=1; i<=n; i++){
+//     for(let j=1; j<=n-i+1; j++){
+//         process.stdout.write("* ");
+//     }
+//     console.log();
+//   }
 
-  for(let i=1; i<=n; i++){
-    for(let j=1; j<=n-i+1; j++){
+
+//OR
+
+
+let n = Number(prompt("Enter the Number: "));
+
+for(let i=1; i<=n; i++){
+    for(let j=n; j>=i; j--){
         // eslint-disable-next-line no-undef
         process.stdout.write("* ");
     }
     console.log();
     
-  }
+}
+
